998ff8da7274ba06635cad34b0c7e721a106ca24
Karol <karl.michalik@gmail.com>
2021-04-24 21:40:07 +0200

    TDD 1st test passed, program returns  a string

---
+++ Wprawka2.Test/FizzBuzzTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wprawka2.Test
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void Buzzer_When1_Returns1()
+        {
+            //Arrange
+            int input = 1;
+
+            //Act
+            string output = FizzBuzz.GetVal(input);
+            //Assert.Pass();
+        }
+    }
+}
---
+++ Wprawka2/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wprawka2
+{
+    public class FizzBuzz
+    {
+        public static string GetVal(int input)
+        {
+            //throw new NotImplementedException();
+            return string.Empty;
+        }
+    }
+}
